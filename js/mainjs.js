a55502ad84a7ac80c1b1abc6659a9ec5683f94f3
juan228a <ruizjuanmartin46@gmail.com>
2024-10-07 00:51:16 -0300

    Modificación del catalogo y creación de catalogo individual y misArticulos

---
+++ js/mainjs.js
@@ -0,0 +1,27 @@
+;(function () {
+ // Obtener elementos del DOM
+ const modal = document.getElementById("myModal");
+ const openModalBtn = document.getElementById("openModalBtn");
+ const closeModalBtn = document.getElementById("closeModalBtn");
+ 
+ // Cuando se hace clic en el botón "+" abre el modal
+ openModalBtn.addEventListener("click", function() {
+     modal.style.display = "block";
+ });
+ 
+ // Cuando se hace clic en el botón "x" cierra el modal
+ closeModalBtn.addEventListener("click", function() {
+    closeModalBtn.onclick = function() {
+        modal.style.display = "none";
+     
+ }});
+
+ 
+ // Cuando se hace clic fuera del contenido del modal, también se cierra
+ window.addEventListener("click", function(event) {
+     if (event.target === modal) {
+         modal.style.display = "none";
+     }
+ });
+
+}());
